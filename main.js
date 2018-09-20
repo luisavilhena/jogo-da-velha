85eda51fc5017a125adcbe6da44436a3c2b49a6d
Habemus <hello@habem.us>
2018-09-20 19:01:14 -0300

    game is working

--- main.js
+++ main.js
@@ -2,19 +2,22 @@
 function setUp() {
   //1-adicionar o evento de click
   //2-colocar a classe bola ou x
-  //2.1- adicionei a var clicked = 0 que soma 1 a cada click
+  //2.1- adicionei a var clickedCalc = 0 que soma 1 a cada click
   //2.2- se o número da clicked for par, add class ball, se ímpar add claxx x
   //verificar se existe uma linha ou coluna clicada
 
-  var clicked = 0
+  var clickedCalc = 0
+  var clickButton = null
   var line1 = 2
   var line2 = 5
   var line3 = 8
   var column1 = [0, 3, 6]
-  console.log(column1.toLocaleString(3))
+
 
 
   var box = Array.from(document.querySelectorAll('button'))
+
+
 
   var boxFirstLine = Array.from(document.querySelectorAll('.first-line'))
   var boxSecondLine = Array.from(document.querySelectorAll('.second-line'))
@@ -22,13 +25,18 @@ function setUp() {
   var boxFirstColumn = Array.from(document.querySelectorAll('.first-column'))
   var boxSecondColumn = Array.from(document.querySelectorAll('.second-column'))
   var boxThirdColumn = Array.from(document.querySelectorAll('.third-column'))
-
+  var boxDiagonalRightLeft = Array.from(document.querySelectorAll('.diagonal-right-left'))
+  var boxDiagonalLeftRight = Array.from(document.querySelectorAll('.diagonal-left-right'))
   console.log(boxFirstColumn)
+
   function aditionClassx (item) {
     item.classList.add('x')
   }
   function aditionClassBall (item) {
     item.classList.add('ball')
+  }
+  function aditionWinner (item) {
+    item.classList.add('winner')
   }
   function boxHasBall (item) {
     return item.classList.contains('ball')
@@ -44,93 +52,155 @@ function setUp() {
   box.forEach(function(button){
 
     button.addEventListener('click', function(){
-
-      clicked++
-      if(clicked%2===0) {
+      ///os  dois jogadores são conforme o par e o ímpar
+      clickedCalc ++
+      if(clickedCalc % 2===0) {
         aditionClassx(button)
       }else{
         aditionClassBall(button)
       }
-      console.log(findPosition(column1))
+      clickButton = this;
 
       /////LINHA 1
-      if(findPosition(button) <= line1) {
-        console.log(findPosition(button))
-        console.log('o')
-  
-        /////jogador x ganha na primeira linha
+
+      if(clickButton.classList.contains('first-line')) {
         var everyLineWithTheClassX = boxFirstLine.every(function (item) {
           return boxHasX(item)
         })
         if (everyLineWithTheClassX===true) {
-          console.log('parabéns x')
+          aditionWinner(button)
         }
-        //////jogador 0 ganha na primeira linha
         var everyLineWithTheClassBall = boxFirstLine.every(function (item) {
           return boxHasBall(item)
         })
         if (everyLineWithTheClassBall===true) {
-          console.log('parabéns ball')
+          boxFirstLine.forEach(aditionWinner(button))
         }
 
 
       //////LINHA 2
-      } else if (findPosition(button) <= line2) {
+      } else if (clickButton.classList.contains('second-line')) {
         var everyLineWithTheClassX = boxSecondLine.every(function (item) {
           return boxHasX(item)
         })
         if (everyLineWithTheClassX===true) {
-          console.log('parabéns x')
+          aditionWinner(button) 
         }
         //////jogador 0 ganha na primeira linha
         var everyLineWithTheClassBall = boxSecondLine.every(function (item) {
           return boxHasBall(item)
         })
         if (everyLineWithTheClassBall===true) {
-          console.log('parabéns ball')
+          aditionWinner(button) 
         }
 
 
       ////////LINHA 3
-      }else if (findPosition(button) <= line3) {
+      }else if (clickButton.classList.contains('third-line')) {
          var everyLineWithTheClassX = boxThirdLine.every(function (item) {
           return boxHasX(item)
         })
         if (everyLineWithTheClassX===true) {
-          console.log('parabéns x')
+          aditionWinner(button) 
         }
         //////jogador 0 ganha na primeira linha
         var everyLineWithTheClassBall = boxThirdLine.every(function (item) {
           return boxHasBall(item)
         })
         if (everyLineWithTheClassBall===true) {
-          console.log('parabéns ball')
+          aditionWinner(button) 
         }
-
+      } 
 
 
       ////////////COLUNA 1
-      } else if (findPosition(button) = boxFirstColumn(button))  {
-        console.log('oooooooooooooo')
-         var everyLineWithTheClassX = boxFirstColumn.every(function (item) {
+      if (clickButton.classList.contains('first-column'))  {
+    
+        var everyColumnWithTheClassX = boxFirstColumn.every(function (item) {
           return boxHasX(item)
         })
-        if (everyLineWithTheClassX===true) {
-          console.log('parabéns x')
+        if (everyColumnWithTheClassX===true) {
+          aditionWinner(button) 
         }
         //////jogador BALL ganha na primeira coluna
-        var everyLineWithTheClassBall = boxFirstColumn.every(function (item) {
+        var everyColumnWithTheClassBall = boxFirstColumn.every(function (item) {
           return boxHasBall(item)
         })
-        if (everyLineWithTheClassBall===true) {
-          console.log('parabéns ball')
+        if (everyColumnWithTheClassBall===true) {
+          aditionWinner(button)
         }
 
-      }else {
 
+        //////////////COLUNA 2
+      }else if(clickButton.classList.contains('second-column')){
+        var everyColumnWithTheClassX = boxSecondColumn.every(function (item) {
+          return boxHasX(item)
+        })
+        if (everyColumnWithTheClassX===true) {
+          aditionWinner(button) 
+        }
+        //////jogador BALL ganha na primeira coluna
+        var everyColumnWithTheClassBall = boxSecondColumn.every(function (item) {
+          return boxHasBall(item)
+        })
+        if (everyColumnWithTheClassBall===true) {
+          aditionWinner(button)
+        }
+
+
+        ////////////COLUNA 3
+      } else if (clickButton.classList.contains('third-column')){
+        var everyColumnWithTheClassX = boxThirdColumn.every(function (item) {
+          return boxHasX(item)
+        })
+        if (everyColumnWithTheClassX===true) {
+          aditionWinner(button) 
+        }
+        //////jogador BALL ganha na primeira coluna
+        var everyColumnWithTheClassBall = boxThirdColumn.every(function (item) {
+          return boxHasBall(item)
+        })
+        if (everyColumnWithTheClassBall===true) {
+          aditionWinner(button)
+        }
       }
+
+      //////////////////DIAGONAL RIGHT LEFT
+      if (clickButton.classList.contains('diagonal-right-left')) {
+        var everyDiagonalRightLeftWithTheClassX = boxDiagonalRightLeft.every(function (item) {
+          return boxHasX(item)
+        })
+        if (everyDiagonalRightLeftWithTheClassX===true) {
+          aditionWinner(button) 
+        }
+        //////jogador BALL ganha na primeira coluna
+        var everyDiagonalRightLeftWithTheClassBall = boxDiagonalRightLeft.every(function (item) {
+          return boxHasBall(item)
+        })
+        if (everyDiagonalRightLeftWithTheClassBall===true) {
+          aditionWinner(button)
+        }
+      }
+
+      ///////////////////DIAGONAL LEFT RIGHT
+      if (clickButton.classList.contains('diagonal-left-right')) {
+        var everyDiagonalLeftRightWithTheClassX = boxDiagonalLeftRight.every(function (item) {
+          return boxHasX(item)
+        })
+        if (everyDiagonalLeftRightWithTheClassX===true) {
+          aditionWinner(button) 
+        }
+        //////jogador BALL ganha na primeira coluna
+        var everyDiagonalLeftRightWithTheClassBall = boxDiagonalLeftRight.every(function (item) {
+          return boxHasBall(item)
+        })
+        if (everyDiagonalLeftRightWithTheClassBall===true) {
+          aditionWinner(button)
+        }
+      }
+      
     })
-    console.log(box.indexOf(button))
+
   })
 }
 
