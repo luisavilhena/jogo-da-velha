685491ef50d2720717dbec0b03ecc30e3bcf5c5d
Habemus <hello@habem.us>
2018-09-27 18:59:09 -0300

    doubt

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 'use strict';
 function setUp() {
+
+  ////problema: preciso criar uma forma que depois que receber a classe winner
+  /////não adicione mais a classe dino 
   /////criar variáveis e funções globais
 
 
@@ -9,6 +12,11 @@ function setUp() {
   var erro = 0
  
   var box = Array.from(document.querySelectorAll('.line > button'))
+  var box2 = Array.from(document.querySelectorAll('.line > button'))
+  // var box3 = box.map(function(){
+  //   return box
+  // })
+ 
   var reset = document.getElementById('reset')
 
   var boxFirstLine = Array.from(document.querySelectorAll('.first-line'))
@@ -48,6 +56,9 @@ function setUp() {
   function boxHasBird (item) {
     return item.classList.contains('bird')
   }
+  function boxHasWinner(item) {
+    return item.classList.contains('winner')
+  }
   // function boxWithoutDino (item) {
   //   return !item.classList.contains('dino')
   // }
@@ -72,11 +83,6 @@ function setUp() {
   function addOnGameClassDisabled() {
     selectGame.classList.add('disabled')
   }
-
-  function chooseOtherBox(){
-     box[Math.floor(Math.random() * box.length)];
-  }
-
   function indexButton(item){
      return box.indexOf(item)
   }
@@ -86,11 +92,14 @@ function setUp() {
   ////retirar classe Dino, Bird, Winner, Disabled de cada click e disabled após terminar o jogo
 
   reset.addEventListener('click', function () {
-    box.forEach(function (button) {
+    box2.forEach(function (button) {
       removeClassDino(button)
       removeClassBird(button)
       removeClassWinner(button)
       removeClassDisabled(button)
+      console.log(removeClassDisabled(button))
+      return box.push(button)
+
     })
     removeOnGameClassDisabled()
   })
@@ -100,38 +109,87 @@ function setUp() {
   box.forEach(function(button){
 
     button.addEventListener('click', function(){
-
+      //se restar apenas 1 item na box, adicione apenas a classe bird
+      if(box.length <= 1) {
+        aditionClassBird(button)
+        return
+      }
+      //Computador é o segundo jogador
       //jogador adiciona a classe Bird
       aditionClassBird(button)
-      //logo depois o código escolhe um randomico para adicionar a classe Dino
+      //  // o código escolhe um randomico para adicionar a classe Dino
       var otherBox= box[Math.floor(Math.random() * box.length)];
-      //retirar do box os elementos que já foram clicados
+
+      
+
+      //quando alguém tiver a classe winner retornar e não fazer mais nada
+      // if(boxHasWinner(button) === true){
+      //   return otherBox.classList.remove('dino')
+      //   console.log('situação1')
+      // }
+      // if(boxHasWinner(otherBox) === true){
+      //   return button.classList.remove('bird')
+      //   console.log('situação2')
+      // }
+     
+      //retirar do box os elementos que já foram clicados com classe bird
       box.splice(indexButton(button), 1)
       //se o box não tiver a classe dino ou bird, pode adicionar a classe dino
-      if(boxHasBird(otherBox)=== false|| boxHasDino(otherBox)===false){
-        aditionClassDino(otherBox)   
-      //retirar do box os elementos com dino 
-        box.splice(indexButton(otherBox), 1)
-      //////////dúvida!!!!!a clase que eu clico não pode receber a classe dino e a bird
-      }else if(otherBox===this){
-        while(otherBox===this){
-          return chooseOtherBox()
-        }
-        aditionClassDino(otherBox)
-      }
 
-      clickedCalc ++
+      if(boxHasBird(otherBox)===false|| boxHasDino(otherBox)===false){
+        //não receber no mesmo button as duas classes
+        if (otherBox ===  this) {        
+          //escolher outro box 
+          var chooseOtherBox = box[Math.floor(Math.random() * box.length)];
+
+          //adicionar a classe dino no outro box
+          aditionClassDino(chooseOtherBox)
+          //se passar pela situação de ser o mesmo e tiver um ganhador, não faça
+          //quando alguém tiver a classe winner retornar e não fazer mais nada
+          // if(boxHasWinner(button) === true){
+          //   return chooseOtherBox.classList.remove('dino')
+          //   console.log('situação3')
+          // }
+          // if(boxHasWinner(chooseOtherBox) === true){
+          //   return button.classList.remove('bird')
+          //   console.log('situação4')
+          // }
+
+          //tirar o box com dino
+          box.splice(indexButton(chooseOtherBox), 1)
+          return
+        }
+        //quando alguém tiver a classe winner retornar e não fazer mais nada
+        //retirar o otherbox do box
+        box.splice(indexButton(otherBox), 1)
+        //colocar a classe dino no box
+        aditionClassDino(otherBox)
+        // if(boxHasWinner(button) === true){
+        //   return otherBox.classList.remove('dino')
+        //   console.log('situação5')
+        // }
+        // if(boxHasWinner(otherBox) === true){
+        //   return button.classList.remove('bird')
+        //   console.log('situação6')
+        // } 
+        
+      }
+    
+
+
+
 
       clickButton = this;
 
       /////adicionar disabled para cada botão clicado e não permitir que seja clicado mais de uma vez
       this.classList.add('disabled')
+      otherBox.classList.add('disabled')
       /////LINHA 1 conferir se ela inteira contém a mesma classe
-      if(clickButton.classList.contains('first-line')) {
+      if(clickButton.classList.contains('first-line') || otherBox.classList.contains('first-line')) {
         everyLineWithTheClassBird = boxFirstLine.every(function (item) {
           return boxHasBird(item)
         })
-        if (everyLineWithTheClassBird === true) {
+        if (everyLineWithTheClassBird ) {
           for (var i = 0; i < boxFirstLine.length; i++) {
             aditionWinner(boxFirstLine[i])
           }
@@ -140,7 +198,7 @@ function setUp() {
         everyLineWithTheClassDino = boxFirstLine.every(function (item) {
           return boxHasDino(item)
         })
-        if (everyLineWithTheClassDino===true) {
+        if (everyLineWithTheClassDino) {
           for (var i = 0; i < boxFirstLine.length; i++) {
             aditionWinner(boxFirstLine[i])
           }
@@ -148,11 +206,12 @@ function setUp() {
         }
 
       //////LINHA 2
-      } else if (clickButton.classList.contains('second-line')) {
+      } 
+      if (clickButton.classList.contains('second-line') || otherBox.classList.contains('second-line')) {
         everyLineWithTheClassBird = boxSecondLine.every(function (item) {
           return boxHasBird(item)
         })
-        if (everyLineWithTheClassBird===true) {
+        if (everyLineWithTheClassBird) {
           for (var i = 0; i < boxSecondLine.length; i++) {
             aditionWinner(boxSecondLine[i])
           }
@@ -161,20 +220,19 @@ function setUp() {
         everyLineWithTheClassDino = boxSecondLine.every(function (item) {
           return boxHasDino(item)
         })
-        if (everyLineWithTheClassDino===true) {
+        if (everyLineWithTheClassDino) {
           for (var i = 0; i < boxSecondLine.length; i++) {
             aditionWinner(boxSecondLine[i])
           }
           addOnGameClassDisabled()
         }
-
-
+      }
       ////////LINHA 3
-      }else if (clickButton.classList.contains('third-line')) {
+      if (clickButton.classList.contains('third-line') || otherBox.classList.contains('third-line')) {
         everyLineWithTheClassBird = boxThirdLine.every(function (item) {
           return boxHasBird(item)
         })
-        if (everyLineWithTheClassBird===true) {
+        if (everyLineWithTheClassBird) {
           for (var i = 0; i < boxThirdLine.length; i++) {
             aditionWinner(boxThirdLine[i])
           } 
@@ -183,7 +241,7 @@ function setUp() {
         everyLineWithTheClassDino = boxThirdLine.every(function (item) {
           return boxHasDino(item)
         })
-        if (everyLineWithTheClassDino===true) {
+        if (everyLineWithTheClassDino) {
           for (var i = 0; i < boxThirdLine.length; i++) {
             aditionWinner(boxThirdLine[i])
           }
@@ -193,12 +251,12 @@ function setUp() {
 
 
       ////////////COLUNA 1
-      if (clickButton.classList.contains('first-column'))  {
+      if (clickButton.classList.contains('first-column')|| otherBox.classList.contains('first-column'))  {
     
         everyColumnWithTheClassBird = boxFirstColumn.every(function (item) {
           return boxHasBird(item)
         })
-        if (everyColumnWithTheClassBird===true) {
+        if (everyColumnWithTheClassBird) {
           for (var i = 0; i < boxFirstColumn.length; i++) {
             aditionWinner(boxFirstColumn[i])
           }
@@ -207,7 +265,7 @@ function setUp() {
         everyColumnWithTheClassDino = boxFirstColumn.every(function (item) {
           return boxHasDino(item)
         })
-        if (everyColumnWithTheClassDino===true) {
+        if (everyColumnWithTheClassDino) {
           for (var i = 0; i < boxFirstColumn.length; i++) {
             aditionWinner(boxFirstColumn[i])
           }
@@ -216,11 +274,12 @@ function setUp() {
 
 
         //////////////COLUNA 2
-      }else if(clickButton.classList.contains('second-column')){
+      }
+      if(clickButton.classList.contains('second-column') || otherBox.classList.contains('second-column')){
         everyColumnWithTheClassBird = boxSecondColumn.every(function (item) {
           return boxHasBird(item)
         })
-        if (everyColumnWithTheClassBird===true) {
+        if (everyColumnWithTheClassBird) {
           for (var i = 0; i < boxSecondColumn.length; i++) {
             aditionWinner(boxSecondColumn[i])
           } 
@@ -229,7 +288,7 @@ function setUp() {
         everyColumnWithTheClassDino = boxSecondColumn.every(function (item) {
           return boxHasDino(item)
         })
-        if (everyColumnWithTheClassDino===true) {
+        if (everyColumnWithTheClassDino) {
           for (var i = 0; i < boxSecondColumn.length; i++) {
             aditionWinner(boxSecondColumn[i])
           }
@@ -238,11 +297,12 @@ function setUp() {
 
 
         ////////////COLUNA 3
-      } else if (clickButton.classList.contains('third-column')){
+      }
+      if (clickButton.classList.contains('third-column') || otherBox.classList.contains('third-column')){
         everyColumnWithTheClassBird = boxThirdColumn.every(function (item) {
           return boxHasBird(item)
         })
-        if (everyColumnWithTheClassBird===true) {
+        if (everyColumnWithTheClassBird) {
           for (var i = 0; i < boxThirdColumn.length; i++) {
             aditionWinner(boxThirdColumn[i])
           }
@@ -251,7 +311,7 @@ function setUp() {
         everyColumnWithTheClassDino = boxThirdColumn.every(function (item) {
           return boxHasDino(item)
         })
-        if (everyColumnWithTheClassDino===true) {
+        if (everyColumnWithTheClassDino) {
           for (var i = 0; i < boxThirdColumn.length; i++) {
             aditionWinner(boxThirdColumn[i])
           }
@@ -260,11 +320,11 @@ function setUp() {
       }
 
       //////////////////DIAGONAL RIGHT LEFT
-      if (clickButton.classList.contains('diagonal-right-left')) {
+      if (clickButton.classList.contains('diagonal-right-left') || otherBox.classList.contains('diagonal-right-left')) {
         everyDiagonalRightLeftWithTheClassBird = boxDiagonalRightLeft.every(function (item) {
           return boxHasBird(item)
         })
-        if (everyDiagonalRightLeftWithTheClassBird===true) {
+        if (everyDiagonalRightLeftWithTheClassBird) {
           for (var i = 0; i < boxDiagonalRightLeft.length; i++) {
             aditionWinner(boxDiagonalRightLeft[i])
           }
@@ -273,7 +333,7 @@ function setUp() {
         everyDiagonalRightLeftWithTheClassDino = boxDiagonalRightLeft.every(function (item) {
           return boxHasDino(item)
         })
-        if (everyDiagonalRightLeftWithTheClassDino===true) {
+        if (everyDiagonalRightLeftWithTheClassDino) {
           for (var i = 0; i < boxDiagonalRightLeft.length; i++) {
             aditionWinner(boxDiagonalRightLeft[i])
           }
@@ -282,11 +342,11 @@ function setUp() {
       }
 
       ///////////////////DIAGONAL LEFT RIGHT
-      if (clickButton.classList.contains('diagonal-left-right')) {
+      if (clickButton.classList.contains('diagonal-left-right') || otherBox.classList.contains('diagonal-left-right')) {
         everyDiagonalLeftRightWithTheClassBird = boxDiagonalLeftRight.every(function (item) {
           return boxHasBird(item)
         })
-        if (everyDiagonalLeftRightWithTheClassBird===true) {
+        if (everyDiagonalLeftRightWithTheClassBird) {
          for (var i = 0; i < boxDiagonalLeftRight.length; i++) {
             aditionWinner(boxDiagonalLeftRight[i])
           }
@@ -295,12 +355,58 @@ function setUp() {
         everyDiagonalLeftRightWithTheClassDino = boxDiagonalLeftRight.every(function (item) {
           return boxHasDino(item)
         })
-        if (everyDiagonalLeftRightWithTheClassDino===true) {
+        if (everyDiagonalLeftRightWithTheClassDino) {
          for (var i = 0; i < boxDiagonalLeftRight.length; i++) {
             aditionWinner(boxDiagonalLeftRight[i])
           }
           addOnGameClassDisabled()
         }
+      }
+
+
+      if(boxHasBird(otherBox)===false|| boxHasDino(otherBox)===false){
+        //não receber no mesmo button as duas classes
+        if (otherBox ===  this) {        
+          //escolher outro box 
+          var chooseOtherBox = box[Math.floor(Math.random() * box.length)];
+
+          //adicionar a classe dino no outro box
+          aditionClassDino(chooseOtherBox)
+          //se passar pela situação de ser o mesmo e tiver um ganhador, não faça
+          //quando alguém tiver a classe winner retornar e não fazer mais nada
+          // if(boxHasWinner(button) === true){
+          //   return chooseOtherBox.classList.remove('dino')
+          //   console.log('situação3')
+          // }
+          // if(boxHasWinner(chooseOtherBox) === true){
+          //   return button.classList.remove('bird')
+          //   console.log('situação4')
+          // }
+          if(boxHasWinner(button)){
+            chooseOtherBox.classList.remove('bird')
+          }
+
+          //tirar o box com dino
+          box.splice(indexButton(chooseOtherBox), 1)
+          return
+        }
+        //quando alguém tiver a classe winner retornar e não fazer mais nada
+        //retirar o otherbox do box
+        box.splice(indexButton(otherBox), 1)
+        //colocar a classe dino no box
+        aditionClassDino(otherBox)
+        if(boxHasWinner(button)){
+          otherBox.classList.remove('bird')
+        }
+        // if(boxHasWinner(button) === true){
+        //   return otherBox.classList.remove('dino')
+        //   console.log('situação5')
+        // }
+        // if(boxHasWinner(otherBox) === true){
+        //   return button.classList.remove('bird')
+        //   console.log('situação6')
+        // } 
+        
       }
     })
   })
